ed16ec2c4ff343c7eb15121c2ca75d3295cfbb0c
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-10-14 13:25:05 -0400

    bound states to form and buttom submit

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -7,34 +7,48 @@ class InputForm extends React.Component {
             idea: "this is the idea txt", // need this to be required
             location: "this is the location txt",
             date: "this is the date txt",  // make this a calander picker or text or both? weekend of xx?
-            budget: 0
+            budget: "this is the budget txt"
             
         }
     }
     update(e){
         this.setState({[e.target.name]: e.target.value})
-    }    
+    }
+    addIdea(e){
+        e.preventDefault();
+        console.log(this);
+    }
     render() {
         return (
             <div>
+                <form onSubmit={this.addIdea.bind(this)}>
+                    <input name="idea" 
+                    placeholder="Idea Name" 
+                    type="text"
+                    onChange={this.update.bind(this)}
+                    />
+                    <input name="location" 
+                    placeholder="Location(optional)" 
+                    type="text"
+                    onChange={this.update.bind(this)}
+                    />
+                    <input name="date" 
+                    placeholder="Date(s)(optional)" 
+                    type="text"
+                    onChange={this.update.bind(this)}
+                    />
+                    <input name="budget" 
+                    placeholder="Budget(optional)" 
+                    type="text"
+                    onChange={this.update.bind(this)}
+                    />
+                    <button type="submit">Submit Idea
+                    </button>
+                </form>
                 <h1>{this.state.idea}</h1>
-                <input name="idea" placeholder="Idea Name" type="text"
-                onChange={this.update.bind(this)}
-                />
                 <h1>{this.state.location}</h1>
-                <input name="location" placeholder="Location(optional)" type="text"
-                onChange={this.update.bind(this)}
-                />
                 <h1>{this.state.date}</h1>
-                <input name="date" placeholder="Date(s)(optional)" type="text"
-                onChange={this.update.bind(this)}
-                />
                 <h1>{this.state.budget}</h1>
-                <input name="budget" placeholder="Budget(optional)" type="number"
-                onChange={this.update.bind(this)}
-                />
-
-                <button>Submit</button>
             </div>
         )
     }
