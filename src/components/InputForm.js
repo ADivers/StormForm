6907a44d3766126c21d1a1e9194540d0ae7acd81
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-10-14 11:27:26 -0400

    e.target.name reusability

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class InputForm extends React.Component {
     constructor(){
         super();
         this.state = {
-            txt: "this is the state txt"
+            idea: "this is the idea txt", // need this to be required
+            location: "this is the location txt",
+            date: "this is the date txt",  // make this a calander picker or text or both? weekend of xx?
+            budget: 0
+            
         }
     }
     update(e){
-        this.setState({txt: e.target.value})
+        this.setState({[e.target.name]: e.target.value})
     }    
     render() {
         return (
             <div>
-                <h1>{this.state.txt}</h1>
-                <input ref="name" placeholder="Idea Name" type="text"
-                onChange={this.update.bind(this)}/>
-                <input ref="location" placeholder="Location(optional)" type="text"/>
-                <input ref="date" placeholder="Date(s)(optional)" type="text"/>
-                <input ref="budget" placeholder="Budget(optional)" type="text"/>
+                <h1>{this.state.idea}</h1>
+                <input name="idea" placeholder="Idea Name" type="text"
+                onChange={this.update.bind(this)}
+                />
+                <h1>{this.state.location}</h1>
+                <input name="location" placeholder="Location(optional)" type="text"
+                onChange={this.update.bind(this)}
+                />
+                <h1>{this.state.date}</h1>
+                <input name="date" placeholder="Date(s)(optional)" type="text"
+                onChange={this.update.bind(this)}
+                />
+                <h1>{this.state.budget}</h1>
+                <input name="budget" placeholder="Budget(optional)" type="number"
+                onChange={this.update.bind(this)}
+                />
+
                 <button>Submit</button>
             </div>
         )
