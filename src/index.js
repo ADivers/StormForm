5a20ebbc78bae240c0fbc9a3ac7986ef24ea4f0b
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-10-14 10:18:14 -0400

    changed inputform location

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
-import InputForm from './InputForm';
+import InputForm from './components/InputForm';
 // import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
