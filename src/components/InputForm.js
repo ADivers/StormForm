744d69e43e484925c0ad8bbd4ff6f23153b420de
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-10-16 20:00:46 -0400

    form functionality done

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,71 +1,84 @@
 import React from 'react';
 
 class InputForm extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            idea: "this is the idea txt", // need this to be required
-            location: "this is the location txt",
-            date: "this is the date txt",  // make this a calander picker or text or both? weekend of xx?
-            budget: "this is the budget txt",
-            ideaPanel: [],
-            
+                idea: "this is the idea txt", // need this to be required
+                location: "this is the location txt",
+                date: "this is the date txt",
+                budget: "this is the budget txt",
+                allInfo: [],
         }
+        this.update = this.update.bind(this)
     }
     update(e){
         this.setState({[e.target.name]: e.target.value})
     }
     addIdea(e){
         e.preventDefault();
-        let idea = this.state.idea;
-        let location = this.state.location;
-        let date = this.state.date;
-        let budget = this.state.budget;
-        // let ideaPanel = [idea, location, date, budget];
 
+        const {idea, location, date, budget, allInfo} = this.state;
+
+        let ideaPanel = {
+            idea,
+            location,
+            date,
+            budget, 
+        };
+
+        const copy = allInfo.slice();
+
+        copy.push(ideaPanel);
         this.setState({
-            ideaPanel: this.state.ideaPanel.concat({idea, location, date, budget})
+            allInfo: copy
         })
-     
-        console.log(this.state.ideaPanel);
-        console.log(this.state);
-        console.log(this.state.idea);
-        console.log(this.state.location);
-        console.log(this.state.date);        
-        console.log(this.state.budget);
     }
     render() { 
+        console.log(this.state)
         return (
             <div>
                 <form onSubmit={this.addIdea.bind(this)}>
                     <input name="idea" 
                     placeholder="Idea Name" 
                     type="text"
-                    onChange={this.update.bind(this)}
+                    onChange={this.update}
                     />
                     <input name="location" 
                     placeholder="Location(optional)" 
                     type="text"
-                    onChange={this.update.bind(this)}
+                    onChange={this.update}
                     />
                     <input name="date" 
                     placeholder="Date(s)(optional)" 
                     type="text"
-                    onChange={this.update.bind(this)}
+                    onChange={this.update}
                     />
                     <input name="budget" 
                     placeholder="Budget(optional)" 
                     type="text"
-                    onChange={this.update.bind(this)}
+                    onChange={this.update}
                     />
                     <button type="submit">Submit Idea
                     </button>
                 </form>
+
+                {
+                    this.state.allInfo.map((i) => {
+                        <ListComponent
+                            name={i.name}
+                            date={i.date}
+                            location={i.location}
+                            budget={i.location}
+                        />
+                    })
+// map over array into ListComponent
+                } 
                 <h1>{this.state.idea}</h1>
                 <h1>{this.state.location}</h1>
                 <h1>{this.state.date}</h1>
                 <h1>{this.state.budget}</h1>
-                <h1>{this.state.ideaPanel}</h1>
+                <h1>{this.state.allInfo.length}</h1>
             </div>
         )
     }
---
+++ src/components/ListComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ListComponent extends React.Component {
+
+ 
+    
+
+
+
+
+
+
+
+    
+}
