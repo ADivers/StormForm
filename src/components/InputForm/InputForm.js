1e7f6e476004f8206c4c6eaf3b36d2c827814376
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-10-25 12:22:58 -0400

    Posts print on HTML

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,181 +1,92 @@
-// import React from 'react';
-// // import Posts from '../Posts/Posts';
-
-// import * as firebase from "firebase";
-
-// import config from './firebase-config';
-
-// class InputForm extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//                 idea: "",
-//                 location: "TBD",
-//                 date: "TBD",
-//                 budget: "TBD",
-//                 allInfo: [],
-//         }
-//         this.update = this.update.bind(this)
-        
-//         firebase.initializeApp(config);
-//     }
-//     update(e){
-//         this.setState({[e.target.name]: e.target.value})
-//     }
-//     addIdea(e){
-//         e.preventDefault();
-
-//         if (this.state.idea === "") {
-//             alert("Please include an idea before submitting.");
-//         } else {
-
-//         const {idea, location, date, budget, allInfo} = this.state;
-
-//         let ideaPanel = {
-//             idea,
-//             location,
-//             date,
-//             budget, 
-//         };
-
-//         const copy = allInfo.slice();
-
-//         copy.push(ideaPanel);
-//         this.setState({
-//             allInfo: copy
-//         });
-
-//         console.log(this);
-
-//         // save to firebase
-
-//         this.state.firebase.ref('posts').push({
-//             title: this.state.title,
-//             location: this.state.location,
-//             date: this.state.date,
-//             budget: this.state.budget,            
-//           });
-      
-
-//     }
-// }
-
-//     render() { 
-//         console.log(this.state)
-//         return (
-//             <div>
-//                 <form onSubmit={this.addIdea.bind(this)}>
-//                     <input name="idea" 
-//                     placeholder="Idea Name" 
-//                     type="text"
-//                     onChange={this.update}
-//                     />
-//                     <input name="location" 
-//                     placeholder="Location(optional)" 
-//                     type="text"
-//                     onChange={this.update}
-//                     />
-//                     <input name="date" 
-//                     placeholder="Date(s)(optional)" 
-//                     type="text"
-//                     onChange={this.update}
-//                     />
-//                     <input name="budget" 
-//                     placeholder="Budget(optional)" 
-//                     type="text"
-//                     onChange={this.update}
-//                     />
-//                     <button type="submit">Submit Idea
-//                     </button>
-//                 </form> 
-                
-//                 {/* {
-//                     this.state.allInfo.map((i) => {
-//                         return <Posts
-//                             idea={i.idea}
-//                             date={i.date}
-//                             location={i.location}
-//                             budget={i.budget}
-//                           />
-//                       })
-//                   }  */}
-//             </div>
-//         )
-//     }
-// }
-
-// export default InputForm;
-
-
-
 import React from 'react';
+import Posts from '../Posts/Posts';
 
-class AddPost extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: '',
-      location: "",
-      date: "",
-      budget: "",
-      allInfo: []      
-    };
-    this.update = this.update.bind(this);
-  }
+class InputForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+                idea: "",
+                location: "TBD",
+                date: "TBD",
+                budget: "TBD",
+                allInfo: [],
+                upvotes: 0,
+        }
+        this.update = this.update.bind(this)
+    }
+    update(e){
+        this.setState({[e.target.name]: e.target.value})
+    }
+    addIdea(e){
+        e.preventDefault();
 
-  update(e){
-    this.setState({[e.target.name]: e.target.value})    
-  }
+        if (this.state.idea === "") {
+            alert("Please include an idea before submitting.");
+        } else {
 
-  addIdea(e){
-    e.preventDefault();
+        const {idea, location, date, budget, allInfo, upvotes} = this.state;
 
-    this.props.firebase.ref('posts').push({
-      title: this.state.title,
-      date: this.state.date,
-      location: this.state.location,
-      budget: this.state.budget,
-      upvote: 0,
-    });
+        let ideaPanel = {
+            idea,
+            location,
+            date,
+            budget,
+            upvotes, 
+        };
 
-    this.setState({
-      title: '',
-      location: "",
-      date: "",
-      budget: ""
-    });
-  }
+        const copy = allInfo.slice();
 
-  render() {
-    return (
-      <AddPost>
-        <form onSubmit={this.addIdea.bind(this)}>
-            <input name="title" 
-            placeholder="Idea Name" 
-            type="text"
-            onChange={this.update}
-            />
-            <input name="location" 
-            placeholder="Location(optional)" 
-            type="text"
-            onChange={this.update}
-            />
-            <input name="date" 
-            placeholder="Date(s)(optional)" 
-            type="text"
-            onChange={this.update}
-            />
-            <input name="budget" 
-            placeholder="Budget(optional)" 
-            type="text"
-            onChange={this.update}
-            />
-            <button type="submit">Submit Idea
-            </button>
-        </form> 
-      </AddPost>
-    );
-  }
+        copy.push(ideaPanel);
+        this.setState({
+            allInfo: copy
+        });
+
+    }
 }
 
-export default AddPost;
+    render(props) { 
+        console.log(this.state)
+        return (
+            <div>
+                <form onSubmit={this.addIdea.bind(this)}>
+                    <input name="idea" 
+                    placeholder="Idea Name" 
+                    type="text"
+                    onChange={this.update}
+                    />
+                    <input name="location" 
+                    placeholder="Location(optional)" 
+                    type="text"
+                    onChange={this.update}
+                    />
+                    <input name="date" 
+                    placeholder="Date(s)(optional)" 
+                    type="text"
+                    onChange={this.update}
+                    />
+                    <input name="budget" 
+                    placeholder="Budget(optional)" 
+                    type="text"
+                    onChange={this.update}
+                    />
+                    <button type="submit">Submit Idea
+                    </button>
+                </form> 
+                
+                {
+                    this.state.allInfo.map((i) => {
+                        return <Posts
+                            idea={i.idea}
+                            date={i.date}
+                            location={i.location}
+                            budget={i.budget}
+                            upvotes={i.upvotes}
+                          />
+                      })
+                  } 
+            </div>
+        )
+    }
+}
+
+export default InputForm;
