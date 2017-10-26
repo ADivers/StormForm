9aaa7f781e20f3a9d35300f106c30cfb4e8cef3e
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-10-25 21:49:51 -0400

    something was wrong with git

---
+++ client/src/components/InputForm/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCyZqCnKHOKXtyLZ7ko9Hkm8FoJyuuMQhk",
+  authDomain: "posts-44222.firebaseapp.com",
+  databaseURL: "https://posts-44222.firebaseio.com",
+  projectId: "posts-44222",
+  storageBucket: "posts-44222.appspot.com",
+  messagingSenderId: "931286045387"
+  };
